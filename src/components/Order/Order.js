97bc340f5656e16f79102f8048c2d636a8160e24
Cristina Tarantino <cristina@tarantino.io>
2019-07-16 15:59:16 +0100

    Outputting the Orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import classes from './Order.module.scss'
 
+import PropTypes from 'prop-types'
+
 const Order = (props) => {
+  const ingredients = []
+
+  for (let ingredientName in props.ingredients) {
+    if (props.ingredients.hasOwnProperty(ingredientName)) {
+      ingredients.push({
+        name: ingredientName,
+        amount: props.ingredients[ingredientName]
+      })
+    }
+  }
+
+  const ingredientOutput = ingredients.map(ig => {
+    return <span
+      style={{
+        textTransform: 'capitalize',
+        display: 'inline-block',
+        margin: '0 8px',
+        padding: '5px'
+      }}
+      key={ig.name}>
+      {ig.name} ({ig.amount})
+    </span>
+  })
+
   return (
     <div className={classes.order}>
-      <p>Ingredients: Salad(1)</p>
-      <p>Price: <strong>USD 5.45</strong></p>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>USD {props.price}</strong></p>
     </div>
   )
+}
+
+Order.prototype = {
+  ingredients: PropTypes.object.isRequired,
+  price: PropTypes.number.isRequired
 }
 
 export default Order
