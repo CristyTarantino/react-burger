f3c1ea91a90c3a09bdbd5edc26fe6fe24dec4007
Cristina Tarantino <cristina@tarantino.io>
2019-07-16 11:30:52 +0100

    build checkout container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Layout from 'hoc/Layout/Layout'
 import BurgerBuilder from 'containers/BurgerBuilder/BurgerBuilder'
+import Checkout from 'containers/Checkout/Checkout'
 
 const app = () => (
   // React.Fragment https://reactjs.org/docs/fragments.html
   <>
     <Layout>
       <BurgerBuilder />
+      <Checkout />
     </Layout>
   </>
 )
---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Burger from 'components/Burger/Burger'
+import Button from 'components/UI/Button/Button'
+
+import classes from './CheckoutSummary.module.scss'
+
+const checkoutSummary = (props) => {
+  return (
+    <div className={classes['checkout-summary']}>
+      <h1>We hope it tastes good!</h1>
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button
+        btnType='danger'
+        clicked>Cancel</Button>
+      <Button
+        btnType='success'
+        clicked>Continue</Button>
+    </div>
+  )
+}
+
+export default checkoutSummary
