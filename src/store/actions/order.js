028670682e08343cde6389f079f7c7c9274ff859
Cristina Tarantino <cristina@tarantino.io>
2019-07-18 16:24:14 +0100

    Fetching Orders (via Redux)

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -37,3 +37,48 @@ export const purchaseBurger = (orderData) => {
 export const purchaseInit = () => ({
   type: actionTypes.PURCHASE_INIT
 })
+
+const fetchOrdersSuccess = (orders) => ({
+  type: actionTypes.FETCH_ORDERS_SUCCESS,
+  payload: {
+    orders: orders
+  }
+})
+
+const fetchOrdersFailed = (error) => ({
+  type: actionTypes.FETCH_ORDERS_FAILED,
+  payload: {
+    error: error
+  }
+})
+
+export const fetchOrdersStart = () => ({
+  type: actionTypes.FETCH_ORDERS_START
+})
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart())
+    axios.get('orders.json')
+      .then(response => {
+        const fetchedOrders = []
+        for (let key in response.data) {
+          if (response.data.hasOwnProperty(key)) {
+            fetchedOrders.push({
+              ...response.data[key],
+              id: key
+            })
+          }
+        }
+
+        dispatch(fetchOrdersSuccess(fetchedOrders))
+      })
+      .catch(error => {
+        dispatch(fetchOrdersFailed(error))
+      })
+  }
+}
+
+export const fetchOrdersInit = () => ({
+  type: actionTypes.FETCH_ORDERS_INIT
+})
