a15a101b228bc45084a312654fb2e515786a64af
Cristina Tarantino <cristina@tarantino.io>
2019-07-18 12:41:31 +0100

    add order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -144,17 +144,7 @@ class ContactData extends Component {
       orderData: formData
     }
 
-    axios.post('/orders.json', order)
-      .then(response => {
-        console.log(response)
-        this.props.history.push('/');
-      })
-      .catch(error => {
-        console.log(error)
-      })
-      .finally(() => {
-        this.setState({loading: false})
-      })
+
   }
 
   inputChangedHandler = (event, formElementId) => {
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,1 +1,30 @@
-// import * as actionTypes from 'store/actions/actionTypes'
+import * as actionTypes from 'store/actions/actionTypes'
+import axios from 'axios-orders'
+
+const purchaseBurgerSuccess = (id, orderData) => ({
+  type: actionTypes.PURCHASE_BURGER_SUCCESS,
+  payload: {
+    id: id,
+    orderData: orderData
+  }
+})
+
+const purchaseBurgerFailed = (error) => ({
+  type: actionTypes.PURCHASE_BURGER_FAILED,
+  payload: {
+    error: error
+  }
+})
+
+export const purchaseBurgerStart = (orderData) => {
+  return dispatch => {
+    axios.post('/orders.json', orderData)
+      .then(response => {
+        console.log(response.data)
+        dispatch(purchaseBurgerSuccess(response.data, orderData))
+      })
+      .catch(error => {
+        dispatch(purchaseBurgerFailed(error))
+      })
+  }
+}
