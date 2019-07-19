fa2bcc7370c5ec1fde161bb65a73f6d552ca11d5
Cristina Tarantino <cristina@tarantino.io>
2019-07-19 15:30:51 +0100

    add logout link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Orders from 'containers/Orders/Orders'
 import BurgerBuilder from 'containers/BurgerBuilder/BurgerBuilder'
 import Checkout from 'containers/Checkout/Checkout'
 import Auth from 'containers/Auth/Auth'
+import Logout from 'containers/Auth/Logout/Logout'
 
 const app = () => (
   // React.Fragment https://reactjs.org/docs/fragments.html
@@ -15,6 +16,7 @@ const app = () => (
         <Route path='/checkout' component={Checkout} />
         <Route path='/orders' component={Orders} />
         <Route path='/auth' component={Auth} />
+        <Route path='/logout' component={Logout} />
         <Route path='/' component={BurgerBuilder} />
       </Switch>
     </Layout>
---
+++ src/containers/Auth/Logout/Logout.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as actions from 'store/actions'
+import { Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+class Logout extends Component {
+  componentDidMount () {
+    this.props.onLogout()
+  }
+
+  render () {
+    return <Redirect to='/' />
+  }
+}
+
+const mapDispatchToProp = dispatch => ({
+  onLogout: () => dispatch(actions.logout())
+})
+
+export default connect(null, mapDispatchToProp)(Logout)
+
+Logout.propTypes = {
+  onLogout: PropTypes.func.isRequired
+}
