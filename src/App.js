5e430838b92c73cda944f1f791e1cad1f3aec19c
Cristina Tarantino <cristina@tarantino.io>
2020-07-03 15:25:06 +0100

    Routing with React.lazy

--- src/App.js
+++ src/App.js
@@ -1,38 +1,72 @@
-import React, {useEffect} from 'react'
-import {Switch, Route} from 'react-router-dom'
+import React, {Suspense, useEffect} from 'react'
+import {Redirect, Route, Switch, withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
 
 import Layout from 'hoc/Layout/Layout'
-import Orders from 'containers/Orders/Orders'
 import BurgerBuilder from 'containers/BurgerBuilder/BurgerBuilder'
-import Checkout from 'containers/Checkout/Checkout'
-import Auth from 'containers/Auth/Auth'
 import Logout from 'containers/Auth/Logout/Logout'
-import {connect} from 'react-redux'
-import * as actions from 'store/actions'
+import * as actions from './store/actions/index'
+
+const Checkout = React.lazy(() => {
+  return import('containers/Checkout/Checkout')
+})
+
+const Orders = React.lazy(() => {
+  return import('containers/Orders/Orders')
+})
+
+const Auth = React.lazy(() => {
+  return import('containers/Auth/Auth')
+})
 
 const App = (props) => {
-  useEffect(() => {
-    props.onTryAutoSignUp()
-  })
+  const {onTryAutoSignUp} = props
 
+  useEffect(() => {
+    onTryAutoSignUp()
+  }, [onTryAutoSignUp])
+
+  let routes = (
+    <Switch>
+      <Route path="/auth" render={() => <Auth />} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  )
+
+  if (props.isAuthenticated) {
+    routes = (
+      <Switch>
+        <Route path="/checkout" render={() => <Checkout />} />
+        <Route path="/orders" render={() => <Orders />} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/auth" render={() => <Auth />} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    )
+  }
   return (
-    // React.Fragment https://reactjs.org/docs/fragments.html
+    // React Fragment
     <>
       <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" component={BurgerBuilder} />
-        </Switch>
+        {/* The lazy component should then be rendered inside a Suspense component,
+            which allows us to show some fallback content (such as a loading indicator)
+            while we’re waiting for the lazy component to load. */}
+        <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
       </Layout>
     </>
   )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  }
 }
 
 const mapDispatchToProps = (dispatch) => ({
   onTryAutoSignUp: () => dispatch(actions.authCheckState()),
 })
 
-export default connect(null, mapDispatchToProps)(App)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
